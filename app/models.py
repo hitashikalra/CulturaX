450fb4d1ab64e35e36b41fb45a5207dbbbd019cd
hi._.tashi <133826452+hitashikalra@users.noreply.github.com>
2024-10-25 22:13:45 -0400

    Create models.py

---
+++ app/models.py
@@ -0,0 +1,16 @@
+from . import db
+
+class Event(db.Model):
+    __tablename__ = 'events'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    location = db.Column(db.String(100), nullable=False)
+
+class Ticket(db.Model):
+    __tablename__ = 'tickets'
+    id = db.Column(db.Integer, primary_key=True)
+    event_id = db.Column(db.Integer, db.ForeignKey('events.id'), nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
+    status = db.Column(db.String(50), nullable=False)
+    event = db.relationship('Event', backref=db.backref('tickets', lazy=True))
